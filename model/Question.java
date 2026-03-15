9afedd41eab571855346c43342838c773b91ea6f
William <williammakolo41@gmail.com>
2026-03-14 22:05:36 -0400

    Added two methods to check the Question was instantiated carefully

--- model/Question.java
+++ model/Question.java
@@ -2,8 +2,20 @@ import java.util.List;
 
 public class Question {
     String questionTest;
-    String answer;
+    String correctAnswer;
     List<String> options;
     Subject subject;
+
+    public boolean isCorrect(String answer) {
+        return this.correctAnswer.equals(answer);
+    }
+
+    public boolean isOption(String option) {
+        return this.options.contains(option);
+    }
+
+    public List<String> generateCopy(){
+        return List.copyOf(this.options);
+    }
 
 }
